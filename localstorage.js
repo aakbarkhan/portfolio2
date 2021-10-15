8b4dee7a57bc507aa69387a1a0639a45fa90c5f6
aakbarkhan <aakuukhan@gmail.com>
2021-10-15 14:24:47 +0530

    Add local storage getitem

--- localstorage.js
+++ localstorage.js
@@ -1,12 +1,27 @@
 // adds stuffs inside localstorage
 function addStorageArr() {
-    const name = document.querySelector('#name_input').value;
-    const email = document.querySelector('#email').value;
-    const message = document.querySelector('#in').value;
-    const storObj = { name, email, message };
-    const store = JSON.stringify(storObj);
-    localStorage.setItem('formData', store);
-    return store;
-  }
+  const name = document.querySelector('#name_input').value;
+  const email = document.querySelector('#email').value;
+  const message = document.querySelector('#in').value;
+  const storObj = { name, email, message };
+  const store = JSON.stringify(storObj);
+  localStorage.setItem('formData', store);
+};
 
-  let dataa = addStorageArr();
+const formInput = document.querySelectorAll('.input-form');
+formInput.forEach((inp) => {
+  inp.addEventListener('input', () => {
+  addStorageArr();
+  });
+});
+
+if (localStorage.getItem('formData') !== null) {
+  let getformData = window.localStorage.getItem('formData');
+  getformData = JSON.parse(getformData);
+  console.log(getformData);
+  document.querySelector('#name_input').value = getformData.name;
+  document.querySelector('#email').value = getformData.email;
+  document.querySelector('#in').value = getformData.message;
+}
+
+  
