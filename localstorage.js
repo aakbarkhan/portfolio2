7ac9e695c054d94cb86cdc96c48dc9f9a262918a
aakbarkhan <aakuukhan@gmail.com>
2021-10-15 16:37:30 +0530

    fix linters error

--- localstorage.js
+++ localstorage.js
@@ -1,20 +1,20 @@
 // adds stuffs inside localstorage
 if (localStorage.getItem('formData') !== null) {
-    let getformData = localStorage.getItem('formData');
-    let stringData = JSON.parse(getformData);
-    document.getElementById('name_input').value = stringData.name;
-    document.getElementById('email').value = stringData.email;
-    document.getElementById('in').value = stringData.message;
+  let getformData = localStorage.getItem('formData');
+  let stringData = JSON.parse(getformData);
+  document.getElementById('name_input').value = stringData.name;
+  document.getElementById('email').value = stringData.email;
+  document.getElementById('in').value = stringData.message;
 }
 function addStorageArr() {
-  let name = document.querySelector('#name_input').value;
-  let email = document.querySelector('#email').value;
-  let message = document.querySelector('#in').value;
-  let storObj = { name, email, message };
-  let store = JSON.stringify(storObj);
+  const name = document.querySelector('#name_input').value;
+  const email = document.querySelector('#email').value;
+  const message = document.querySelector('#in').value;
+  const storObj = { name, email, message };
+  const store = JSON.stringify(storObj);
   localStorage.setItem('formData', store);
 }
-let formInput = document.querySelectorAll('.input-form');
+const formInput = document.querySelectorAll('.input-form');
 formInput.forEach((inp) => {
   inp.addEventListener('input', () => {
     addStorageArr();
