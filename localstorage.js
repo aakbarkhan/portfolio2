aa9749aed236702b18efab9a34efea9d784a709a
aakbarkhan <aakuukhan@gmail.com>
2021-10-15 16:29:23 +0530

    fix local storage

--- localstorage.js
+++ localstorage.js
@@ -1,22 +1,22 @@
 // adds stuffs inside localstorage
+if (localStorage.getItem('formData') !== null) {
+    let getformData = localStorage.getItem('formData');
+    let stringData = JSON.parse(getformData);
+    document.getElementById('name_input').value = stringData.name;
+    document.getElementById('email').value = stringData.email;
+    document.getElementById('in').value = stringData.message;
+}
 function addStorageArr() {
-  const name = document.querySelector('#name_input').value;
-  const email = document.querySelector('#email').value;
-  const message = document.querySelector('#in').value;
-  const storObj = { name, email, message };
-  const store = JSON.stringify(storObj);
+  let name = document.querySelector('#name_input').value;
+  let email = document.querySelector('#email').value;
+  let message = document.querySelector('#in').value;
+  let storObj = { name, email, message };
+  let store = JSON.stringify(storObj);
   localStorage.setItem('formData', store);
 }
-const formInput = document.querySelectorAll('.input-form');
+let formInput = document.querySelectorAll('.input-form');
 formInput.forEach((inp) => {
   inp.addEventListener('input', () => {
     addStorageArr();
   });
 });
-if (localStorage.getItem('formData') !== null) {
-  let getformData = window.localStorage.getItem('formData');
-  getformData = JSON.parse(getformData);
-  document.querySelector('#name_input').value = getformData.name;
-  document.querySelector('#email').value = getformData.email;
-  document.querySelector('#in').value = getformData.message;
-}
